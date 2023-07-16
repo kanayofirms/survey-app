423038246ef8bb8fa5ca2368e370c8266752342f
kanayofirms <kanyeze@gmail.com>
2023-07-16 07:54:25 +0100

    Create and use store function

--- survey-app/vue/src/main.js
+++ survey-app/vue/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import store from './store'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(store)
+.mount('#app')
+
