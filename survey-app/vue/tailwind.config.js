2203b0f4e362f07d622f2a5f79ff90c333f1ec78
kanayofirms <kanyeze@gmail.com>
2023-07-17 06:09:24 +0100

    Update plugin for functionality

--- survey-app/vue/tailwind.config.js
+++ survey-app/vue/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"
+],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
