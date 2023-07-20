e41efe57e62e93928dcb074181d46f9fa63e3301
kanayofirms <kanyeze@gmail.com>
2023-07-20 07:10:08 +0100

    Initial commit

---
+++ survey-app/vue/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory,
+    routes
+})
+
+export default router;
