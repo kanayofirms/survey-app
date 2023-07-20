fc02815cd1ad331f62f3b406e1d107ae386b983d
kanayofirms <kanyeze@gmail.com>
2023-07-20 07:08:59 +0100

    Import and use router

--- survey-app/vue/src/main.js
+++ survey-app/vue/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import'./index.css'
 import store from './store'
+import router from './router'
 import App from './App.vue'
 
 createApp(App)
 .use(store)
+.use(router)
 .mount('#app')
 
