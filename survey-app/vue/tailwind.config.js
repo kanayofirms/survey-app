a110ad7c193c3eee51562c86a48bbf0afe1142b5
kanayofirms <kanyeze@gmail.com>
2023-07-26 11:55:11 +0100

    Create and configure purge and plugin

--- survey-app/vue/tailwind.config.js
+++ survey-app/vue/tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"
-],
+export default  {
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  purge: [ 
+    './src/**/*.html', 
+    './src/**/*.vue', 
+    './src/**/*.jsx', 
+  ],
   theme: {
     extend: {},
   },
