499d4413c7f29a8c3ded805a8087e77b150aecd2
kanayofirms <kanyeze@gmail.com>
2023-07-27 16:50:09 +0100

    Delete purge from config file

--- survey-app/vue/tailwind.config.js
+++ survey-app/vue/tailwind.config.js
@@ -4,11 +4,7 @@ export default  {
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  purge: [ 
-    './src/**/*.html', 
-    './src/**/*.vue', 
-    './src/**/*.jsx', 
-  ],
+
   theme: {
     extend: {},
   },
