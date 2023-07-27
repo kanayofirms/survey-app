9f105f8f244ef34a1abc4456cb6d9c3695ed9f52
kanayofirms <kanyeze@gmail.com>
2023-07-27 17:01:02 +0100

    Edit and update Dashboard page

--- survey-app/vue/src/views/Dashboard.vue
+++ survey-app/vue/src/views/Dashboard.vue
@@ -1,13 +1,20 @@
 <template>
-    <h1>Dashboard</h1>
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <!-- Your content -->
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-    name: "Dashboard"
-}
+  name: "Dashboard",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
