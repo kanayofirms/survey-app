69a8c731bc003a317624e4c31928f0a54f99d60d
kanayofirms <kanyeze@gmail.com>
2023-07-27 17:03:49 +0100

    Update routes with DefaultLayout component and create route paths

--- survey-app/vue/src/router/index.js
+++ survey-app/vue/src/router/index.js
@@ -1,13 +1,19 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
+import Surveys from '../views/Surveys.vue';
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue"; 
+import DefaultLayout from "../components/DefaultLayout.vue";
 
 const routes = [
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard
+        redirect: '/dashboard',
+        component: DefaultLayout,
+        children: [
+            {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+            {path: '/surveys', name: 'Surveys', component: Surveys}
+        ]
     },
     {
         path: '/login',
